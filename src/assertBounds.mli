42b76f1641fa3525e2186ae5288e1e336af5837e
Pierre Villemot <tiky.halbaroth@gmail.com>
2022-11-10 22:38:18 +0100

    Documentation peotry

--- src/assertBounds.mli
+++ src/assertBounds.mli
@@ -8,13 +8,12 @@ module type S = sig
 
   module Core : CoreSig.S
 
-  (* The returned bool is true if the asserted bounds are not trivial
-     (i.e. not implied by known bounds) *)
-  (** [var env min max x]
-      From an environment `env`, returns an environment `env'` in which the
-      bounds of `x` are updated to `min` and `max`.
+  (*  The returned bool is [true] if the asserted bounds are not trivial
+      (i.e. not implied by known bounds) *)
+  (** [var env min max x] returns a new environment obtained by changing
+      the bounds of [x] in [env] to [min] and [max].
       If the bounds were implied by other known bounds (in other words, if the
-      environment did not change) the associated boolean will be `false`.
+      environment did not change) the associated boolean will be [false].
   *)
   val var :
     Core.t ->
@@ -23,14 +22,13 @@ module type S = sig
     Core.Var.t ->
     Core.t * bool
 
-  (* The returned bool is true if the asserted bounds are not trivial
-     (i.e. not implied by known bounds) *)
-  (** [poly env poly min max x]
-      From an environment `env`, returns an environment `env'` in which the
-      bounds of `poly` are updated to `min` and `max`. The polynomial is represented
-      by the slack variable `x`.
+  (*  The returned bool is [true] if the asserted bounds are not trivial
+      (i.e. not implied by known bounds) *)
+  (** [poly env poly min max x] returns a new environment obtained by changing
+      the bounds of [poly] in [env] to [min] and [max].
+      The polynomial is represented by the slack variable [x].
       If the bounds were implied by other known bounds (in other words, if the
-      environment did not change) the associated boolean will be `false`.
+      environment did not change) the associated boolean will be [false].
   *)
   val poly :
     Core.t ->
--- src/coreSig.mli
+++ src/coreSig.mli
@@ -6,8 +6,10 @@
 
 open ExtSigs
 
-(** Interface of the main types and auxiliary of the simplex *)
+(** Interface of the main types and auxiliary of the simplex. *)
 module type S = sig
+
+  (** {1 Modules} *)
 
   (** The type of variables maipulated by the simplex algorithm. *)
   module Var : Variables
@@ -23,42 +25,43 @@ module type S = sig
   (** The interface for rationals provided by users. *)
   module R   : Rationals
 
-  (** Pairs of rationals R representing bounds with an offset (x + kƐ). *)
+  (** Pairs of rationals R representing bounds with an offset [x + kƐ]. *)
   module R2  : Rat2.SIG with module R = R
 
   (** Linear relations of variables. *)
   module P : Polys.SIG with module Var = Var and module R = R
 
-  (** Collections of variables *)
+  (** Collections of variables. *)
   module MX : Map.S with type key = Var.t
   module SX : Set.S with type elt = Var.t
 
   (*module SLAKE : Map.S with type key = P.t*)
 
-  (** A bound is a value (x + kƐ) and an explanation. *)
+  (** {1 Types} *)
+  
+  (** A bound is a value of the form [x + kƐ] and an explanation. *)
   type bound = {
     bvalue : R2.t;
-    explanation: Ex.t
+    explanation : Ex.t
   }
 
   type value_status =
-    | ValueOK (** The value is inbetween bounds *)
-    | LowerKO (** The value is smaller than the lower bound *)
-    | UpperKO (** The value is greater than the upper bound *)
+    | ValueOK (** The value is inbetween bounds. *)
+    | LowerKO (** The value is smaller than the lower bound. *)
+    | UpperKO (** The value is greater than the upper bound. *)
 
-  type var_info =
-    {
-      mini     : bound option; (* None -> -inf *)
-      maxi     : bound option; (* None -> +inf *)
-      value    : R2.t;
-      vstatus  : value_status;
+  type var_info = {
+      mini : bound option; (* None -> -inf *)
+      maxi : bound option; (* None -> +inf *)
+      value : R2.t;
+      vstatus : value_status;
       empty_dom : bool;
     }
 
   type solution = {
     main_vars : (Var.t * R.t) list;
     slake_vars : (Var.t * R.t) list;
-    int_sol : bool; (* always set to false for rational simplexes*)
+    int_sol : bool; (* Always set to false for rational simplexes. *)
     epsilon : R.t;
   }
 
@@ -76,76 +79,72 @@ module type S = sig
 
   type simplex_status = UNK | UNSAT of Var.t | SAT
 
-  type t =
-    {
-      basic     : (var_info * P.t) MX.t;
+  type t = {
+      basic : (var_info * P.t) MX.t;
       non_basic : (var_info * SX.t) MX.t;
-      slake     : P.t MX.t;
-      fixme     : SX.t;
-      is_int    : bool;
-      status    : simplex_status;
-      check_invs: bool;
+      slake : P.t MX.t;
+      fixme : SX.t;
+      is_int : bool;
+      status : simplex_status;
+      check_invs : bool;
       nb_pivots : int ref;
     }
 
   (** Returns a simplex environment with three parameters:
-      - `is_int`: will the simplex work on an integer optimization problem or a
+      - [is_int]: will the simplex work on an integer optimization problem or a
         rational problem?
-      - `check_invs`: processes checks after the calculation (deprecated)
+      - [check_invs]: processes checks after the calculation (deprecated).
   *)
   val empty : is_int : bool -> check_invs : bool -> t
 
-  (** Returns true if the simplex environment is on integers. *)
+  (** Returns [true] if the simplex environment is on integers. *)
   val on_integers : t -> bool
 
-  (** Equality check betweeb bounds. *)
+  (** Equality check between bounds. *)
   val equals_optimum : R2.t -> bound option -> bool
 
-  (** Checks if the lower bound of a variable is smaller than its upper bound. *)
+  (** Checks if the lower bound of a variable is smaller than
+      its upper bound. *)
   val consistent_bounds : var_info -> bool
 
-  (** [violates_min_bound b mb]
-      Returns true if `b` is smaller than mb. *)
+  (** [violates_min_bound b mb] returns [true] if [b] is smaller than [mb]. *)
   val violates_min_bound : R2.t -> bound option -> bool
 
-  (** [violates_max_bound b mb]
-      Returns true if `b` is greater than mb. *)
+  (** [violates_max_bound b mb] returns [true] if [b] is greater than [mb]. *)
   val violates_max_bound : R2.t -> bound option -> bool
 
-  (* The returned bool is true if the asserted bounds are not trivial
-     (i.e. not implied by known bounds) *)
-  (** [set_min_bound vinfo b]
-      Returns a couple `(vinfo', changed)` where:
-      - `vinfo'` is the new variable info where the new min bound `b` has
-        been set
-      - `changed` is `true` if the new bound has changed the variable info
+  (* The returned bool is [true] if the asserted bounds are not trivial
+      (i.e. not implied by known bounds). *)
+  (** [set_min_bound vinfo b] returns a couple [(vinfo', changed)] where:
+      - [vinfo'] is the new variable info where the new min bound [b] has
+        been set.
+      - [changed] is [true] if the new bound has changed the variable info
   *)
   val set_min_bound : var_info -> bound option -> var_info * bool
 
-  (** Same as `set_min_bound`, but for max bounds. *)
+  (** Same as {!val:set_min_bound}, but for max bounds. *)
   val set_max_bound : var_info -> bound option -> var_info * bool
 
-  (** [ajust_value_of_non_basic vinfo]
-      If `vinfo`'s status is UpperKO (resp. LowerKO), updates
-      the variable info's value with the upper bound (rest. the lower bound).
-      Otherwise, do nothing.
-      The boolean returnes is `true` if the new `var_info` has changed. *)
+  (** [ajust_value_of_non_basic vinfo] updates the info's value with the upper
+      bound (resp. the lower bound), if [vinfo]'s status is {!const:UpperKO}
+      (resp. {!const:LowerKO}). Otherwise, do nothing.
+      The boolean returned is [true] if the new variable [var_info]
+      has changed. *)
   val ajust_value_of_non_basic: var_info -> var_info * bool
   (* vstatus is supposed to be well set *)
 
-  (** [ajust_status_of_basic vinfo]
-      Checks a variable info's bound matches its status. If its value violates
-      its lower bound, its status is set to LowerKO.
-      In the other case, it is set to UpperKO.
-      If the value is between the two bounds, it is set to ValueOK. *)
+  (** [ajust_status_of_basic vinfo] checks a variable info's bound matches
+      its status. If its value violates its lower bound, its status is set
+      to {!const:LowerKO}. In the other case, it is set to {!const:UpperKO}.
+      If the value is between the two bounds, it is set to {!const:ValueOK}. *)
   val ajust_status_of_basic : var_info -> var_info
   (* valuation is supposed to be well computed *)
 
   (** Evaluates a polynomial of non basic variables. *)
   val evaluate_poly : t -> P.t -> R2.t
 
-  (** [poly_of_slake env slake]
-      Returns the polynomial associated to the variable `slake` in `env`. *)
+  (** [poly_of_slake env slake] returns the polynomial associated
+      to the variable [slake] in [env]. *)
   val poly_of_slake : t -> Var.t -> P.t option
 
   (** Returns the explanation associated to a variable lower bound. *)
@@ -154,7 +153,8 @@ module type S = sig
   (** Same as `expl_of_min_bound`, but for upper bounds. *)
   val expl_of_max_bound : var_info -> Ex.t
 
-  (* debug functions and invariants; only use for internal debugging *)
+  (** {1 Debug functions} *)
+  (** Only use for internal debugging *)
 
   (** Checks several invariants in the project *)
   val check_invariants : t -> (t -> result) -> unit
@@ -162,6 +162,6 @@ module type S = sig
   (** Pretty prints the environment. *)
   val print : result -> Format.formatter -> t -> unit
 
-  (** (deprecated) *)
+  (** @deprecated *)
   val debug : string -> t -> (t -> result) -> unit
 end
